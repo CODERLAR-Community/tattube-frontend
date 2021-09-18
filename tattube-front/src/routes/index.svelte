cd7626a999a0792c9e8fe044f840b64e3bac93dd
Ural <ural2001@gmail.com>
2021-09-18 15:21:48 +0300

    add channels page

--- tattube-front/src/routes/index.svelte
+++ tattube-front/src/routes/index.svelte
@@ -62,139 +62,6 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v2.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-success">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v3.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-danger">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v4.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-success">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v5.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-success">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v6.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-danger">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v7.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-success">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="video-card">
-						<div class="video-card-image">
-							<a class="play-icon" href="#"><i class="fas fa-play-circle"></i></a>
-							<a href="#"><img class="img-fluid" src="/static/img/v8.png" alt=""></a>
-							<div class="time">3:50</div>
-						</div>
-						<div class="video-card-body">
-							<div class="video-title">
-								<a href="#">There are many variations of passages of Lorem</a>
-							</div>
-							<div class="video-page text-success">
-							</div>
-							<div class="video-view">
-								1.8M просмотров &nbsp;<i class="fas fa-calendar-alt"></i> 11 месяцев назад
-							</div>
-						</div>
-					</div>
-				</div>
 			</div>
 		</div>
 		<hr class="mt-0">
@@ -202,16 +69,6 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="main-title">
-<!--						<div class="btn-group float-right right-action">-->
-<!--							<a href="#" class="right-action-link text-gray" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
-<!--								Sort by <i class="fa fa-caret-down" aria-hidden="true"></i>-->
-<!--							</a>-->
-<!--							<div class="dropdown-menu dropdown-menu-right">-->
-<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-star"></i> &nbsp; Top Rated</a>-->
-<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-signal"></i> &nbsp; Viewed</a>-->
-<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-times-circle"></i> &nbsp; Close</a>-->
-<!--							</div>-->
-<!--						</div>-->
 						<h6>Каналы</h6>
 					</div>
 				</div>
@@ -234,36 +91,6 @@
 					<div class="channels-card">
 						<div class="channels-card-image">
 							<a href="#"><img class="img-fluid" src="/static/img/s2.png" alt=""></a>
-							<div class="channels-card-image-btn"><button type="button" class="btn btn-outline-danger btn-sm">Подписаться <strong>1.4M</strong></button></div>
-						</div>
-						<div class="channels-card-body">
-							<div class="channels-title">
-								<a href="#">Название канала</a>
-							</div>
-							<div class="channels-view">
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="channels-card">
-						<div class="channels-card-image">
-							<a href="#"><img class="img-fluid" src="/static/img/s3.png" alt=""></a>
-							<div class="channels-card-image-btn"><button type="button" class="btn btn-outline-secondary btn-sm">Отписаться <strong>1.4M</strong></button></div>
-						</div>
-						<div class="channels-card-body">
-							<div class="channels-title">
-								<a href="#">Название канала <span title="" data-placement="top" data-toggle="tooltip" data-original-title="Verified"><i class="fas fa-check-circle"></i></span></a>
-							</div>
-							<div class="channels-view">
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-3 col-sm-6 mb-3">
-					<div class="channels-card">
-						<div class="channels-card-image">
-							<a href="#"><img class="img-fluid" src="/static/img/s4.png" alt=""></a>
 							<div class="channels-card-image-btn"><button type="button" class="btn btn-outline-danger btn-sm">Подписаться <strong>1.4M</strong></button></div>
 						</div>
 						<div class="channels-card-body">
