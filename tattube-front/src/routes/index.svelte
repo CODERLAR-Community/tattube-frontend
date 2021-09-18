d74a509411112647f97fb2798b2daad2c3f19cc6
Ural <ural2001@gmail.com>
2021-09-18 13:00:47 +0300

    add new colors

--- tattube-front/src/routes/index.svelte
+++ tattube-front/src/routes/index.svelte
@@ -30,16 +30,16 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="main-title">
-						<div class="btn-group float-right right-action">
-							<a href="#" class="right-action-link text-gray" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Sort by <i class="fa fa-caret-down" aria-hidden="true"></i>
-							</a>
-							<div class="dropdown-menu dropdown-menu-right">
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-star"></i> &nbsp; Top Rated</a>
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-signal"></i> &nbsp; Viewed</a>
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-times-circle"></i> &nbsp; Close</a>
-							</div>
-						</div>
+<!--						<div class="btn-group float-right right-action">-->
+<!--							<a href="#" class="right-action-link text-gray" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--								Sort by <i class="fa fa-caret-down" aria-hidden="true"></i>-->
+<!--							</a>-->
+<!--							<div class="dropdown-menu dropdown-menu-right">-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-star"></i> &nbsp; Top Rated</a>-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-signal"></i> &nbsp; Viewed</a>-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-times-circle"></i> &nbsp; Close</a>-->
+<!--							</div>-->
+<!--						</div>-->
 						<h6>Видео</h6>
 					</div>
 				</div>
@@ -202,17 +202,17 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="main-title">
-						<div class="btn-group float-right right-action">
-							<a href="#" class="right-action-link text-gray" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Sort by <i class="fa fa-caret-down" aria-hidden="true"></i>
-							</a>
-							<div class="dropdown-menu dropdown-menu-right">
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-star"></i> &nbsp; Top Rated</a>
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-signal"></i> &nbsp; Viewed</a>
-								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-times-circle"></i> &nbsp; Close</a>
-							</div>
-						</div>
-						<h6>Popular Channels</h6>
+<!--						<div class="btn-group float-right right-action">-->
+<!--							<a href="#" class="right-action-link text-gray" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--								Sort by <i class="fa fa-caret-down" aria-hidden="true"></i>-->
+<!--							</a>-->
+<!--							<div class="dropdown-menu dropdown-menu-right">-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-star"></i> &nbsp; Top Rated</a>-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-signal"></i> &nbsp; Viewed</a>-->
+<!--								<a class="dropdown-item" href="#"><i class="fas fa-fw fa-times-circle"></i> &nbsp; Close</a>-->
+<!--							</div>-->
+<!--						</div>-->
+						<h6>Каналы</h6>
 					</div>
 				</div>
 				<div class="col-xl-3 col-sm-6 mb-3">
