0ade1018d147d4747af5f2033629b40d827c2faf
Ural <ural2001@gmail.com>
2021-09-18 13:15:13 +0300

    add 404 page

---
+++ tattube-front/src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Header from '$lib/header/Header.svelte';
+    import SideBar from '$lib/sideBar/SideBar.svelte';
+    import Footer from '$lib/footer/Footer.svelte';
+</script>
+
+<div style="display: flex; justify-content: center">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 mx-auto text-center  pt-4 pb-5">
+                <h1>
+<!--                    <img alt="404" src="/static/img/404.png" class="img-fluid">-->
+                    Ошибка 404
+                </h1>
+                <h1>Страница не найдена.</h1>
+                <p class="land">К сожалению страница, которую вы ищите, не существует.</p>
+                <div class="mt-5">
+                    <a class="btn btn-outline-primary" href="/"><i class="mdi mdi-home"></i> Вернуться на главную</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
