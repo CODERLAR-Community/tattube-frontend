17b6c730c8eb831f9230c7bfa6be3ab78928a685
Ural <ural2001@gmail.com>
2021-09-21 21:22:21 +0300

    add adapter

--- tattube-front/svelte.config.js
+++ tattube-front/svelte.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
+import node from '@sveltejs/adapter-node'
+// import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
@@ -9,12 +10,7 @@ const config = {
 				exclude: ['@urql/svelte'],
 			}
 		},
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build/static',
-			fallback: null
-		})
+		adapter: node(),
 	}
 };
 
